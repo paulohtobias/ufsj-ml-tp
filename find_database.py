c36548b47eef1118b10ccc6521880346e2406204
Millas N <millas5444@gmail.com>
2018-09-02 16:36:53 -0300

    Adição do programa que captura os n top animes do MyAnimeList
    - Adição da função fill_database: ela retorna uma lista com o link dos animes no ranking
    obs: Os links retornados não possuem o http://myanimelist.net no início

---
+++ find_database.py
@@ -0,0 +1,27 @@
+import crawler
+
+import urllib2
+from bs4 import BeautifulSoup as bs
+
+#definicoes
+MAL_URL = "https://myanimelist.net"
+
+def fill_database(start_page, num_pages):
+	anime_links = []
+
+	for i in [x*50 for x in range(start_page,start_page + num_pages)]:
+		# Capturando a pagina do MyAnimeList
+		url = MAL_URL + "/topanime.php?limit=" + str(i)
+		response = urllib2.urlopen(url.encode("UTF-8"))
+		pagina = response.read()
+		soup = bs(pagina, "html5lib")
+
+		# Capturando todos os links com o link dos animes
+		links = soup.find_all("a", {"class":"hoverinfo_trigger fl-l ml12 mr8"})
+
+		# Adicionando lista de animes na pagina atual a 
+		# lista global de animes
+		anime_links.extend([x.get("href")[len(MAL_URL):] for x in links])
+
+	return anime_links
+print fill_database(0,5)
