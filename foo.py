7dc4bf95789dc83d5341ff682adcd8fa8bab1e90
Paulo Tobias <paulohtobias@outlook.com>
2018-09-07 11:23:32 -0300

    foo.py alterado para baixar as paginas restantes

--- foo.py
+++ foo.py
@@ -3,7 +3,19 @@
 import os
 intervalo = 5
 
-for pagina_base in range(0, 100, intervalo):
+pagina_inicio = 0
+pagina_fim = pagina_inicio + 100
+
+paginas = range(pagina_inicio, pagina_fim, intervalo)
+
+paginas_faltando = sorted(map(lambda n: int(filter(str.isdigit, n)), os.listdir("log")))
+
+if len(paginas_faltando) > 0:
+	paginas = paginas_faltando
+
+print pagina_inicio, pagina_fim, intervalo
+
+for pagina_base in paginas_faltando:
 	print "################### PAGINA BASE: " + str(pagina_base) + " ###################"
 	comando = "./anime_crawler.sh %d %d" % (pagina_base, pagina_base + intervalo)
 	
