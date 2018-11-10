49cd1c5d489e299f90c36da5067e5d6fbc7b1511
Paulo Tobias <paulohtobias@outlook.com>
2018-11-10 16:55:55 -0200

    site

--- Interface/js/index.js
+++ Interface/js/index.js
@@ -1,3 +1,4 @@
+/****************** Informações Globais ******************/
 var valor_estimativa = 0.5;
 
 var starCap = {
@@ -26,3 +27,28 @@ var starCapClass = {
 	4.5: 'badge badge-success bg-success f-md',
 	5.0: 'badge badge-success bg-success f-md'
 };
+
+/************************ Funções ************************/
+$("#avaliar").click(function(){
+	$("#estimativa").attr("value",valor_estimativa);
+	$("#estimativa").rating({
+		disabled: true,
+		clearButton: "",
+		size: "sm",
+		captionElement: "#caption-estimativa",
+		starCaptions: starCap,
+		starCaptionClasses: starCapClass
+	});
+	$.ajax({
+		type: "POST",
+		url: "api_python_avaliacao.php",
+		data: { 
+			url: "",
+			user: ""
+		}
+	}).done(function( msg ) {
+		alert( "Data Saved: " + msg );
+	});
+	$("#estimativa").attr("value",5);
+	location.reload(true);
+});
